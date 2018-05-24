3d7a20e0cce1030aab4fda9e84465798f1f21d20
Tim <timothy@ultratesting.us>
2018-05-24 14:26:00 -0500

    added comments

--- losscost.py
+++ losscost.py
@@ -1,14 +1,16 @@
 import xlrd 
 import openpyxl 
 
+# load input file
 wb = xlrd.open_workbook("c:\losscost\iowa.xlsx")
 ws = wb.sheet_by_name('Table 1')
 
-file = 'c:\losscost\output.xlsx'
-nb = openpyxl.load_workbook(filename=file)
+# load output file
+outfile = 'c:\losscost\output.xlsx'
+nb = openpyxl.load_workbook(filename=outfile)
 ns = nb['Output']
-nRow = 0
 
+# function to get data from input file and write to output file
 def codeSearch(x):
     for row in range(ws.nrows):
         for col in range(ws.ncols):
@@ -21,10 +23,11 @@ def codeSearch(x):
                 ns.cell(row=nRow, column=2, value=b)
                 ns.cell(row=nRow, column=3, value=c)
                 
-
+# reads code list and searches for each one
 data = xlrd.open_workbook("c:\losscost\codes.xlsx")
 sheet = data.sheet_by_name('codes')
 for row in range(sheet.nrows):
     codeSearch(sheet.cell_value(row,0))
-    
-nb.save(file)
+
+# save output file    
+nb.save(outfile)
